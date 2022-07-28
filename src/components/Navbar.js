a6b6747e18dc068c399a618ad43b4590356f9c28
Arch0wl <annstarovoitov@gmail.com>
2022-07-28 11:25:21 -0400

    navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
@@ -30,7 +44,7 @@ function Navbar() {
               Services
             </Link>
           </li>
-          <li className="nav-item">
+          <ul className="nav-item">
             <Link
               to="/products"
               className="nav-links"
@@ -47,7 +61,8 @@ function Navbar() {
                 Sign Up
               </Link>
             </li>
-          </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
